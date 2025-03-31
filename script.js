002b020059ebf631d07fad1fa3d59d95ec25f4d2
bberker06 <152335565+bberker06@users.noreply.github.com>
2025-03-31 16:34:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,20 +18,35 @@ document.getElementById("find-borough-btn").addEventListener("click", function()
 
                 // List of valid boroughs you want to match (simplified)
                 var boroughsList = [
-                    "Aberdeen City", "Adur", "Allerdale", "Amber Valley", "Angus", "Argyll and Bute", "Arun", "Ashfield", 
-                    "Ashford", "Aylesbury Vale", "Babergh", "Barking and Dagenham", "Barnet", "Barnsley", "Barrow-in-Furness",
-                    "Basildon", "Basingstoke and Deane", "Bassetlaw", "Bath and North East Somerset", "Bedford", "Bexley",
-                    "Birmingham", "Blackburn with Darwen", "Blackpool", "Blaenau Gwent", "Bolton", "Bournemouth", "Bracknell Forest",
-                    "Bradford", "Braintree", "Bromley", "Camden", "Canterbury", "Cardiff", "Cheshire West and Chester", "Croydon",
-                    "Derby", "Doncaster", "Dudley", "Enfield", "Hackney", "Haringey", "Hillingdon", "Islington", "Lewisham",
-                    "Manchester", "Merton", "Newham", "Northumberland", "Nottingham", "Redbridge", "Richmond upon Thames", 
-                    "Southwark", "Sutton", "Tower Hamlets", "Waltham Forest", "Wandsworth", "Westminster"
+                    "Aberdeen City", "Aberdeenshire", "Adur", "Allerdale", "Amber Valley", "Angus", "Argyll and Bute", "Arun", 
+                    "Ashfield", "Ashford", "Aylesbury Vale", "Babergh", "Barking and Dagenham", "Barnet", "Barnsley", "Barrow-in-Furness",
+                    "Basildon", "Basingstoke and Deane", "Bassetlaw", "Bath and North East Somerset", "Bedford", "Bexley", "Birmingham",
+                    "Blaby", "Blackburn with Darwen Borough Council", "Blackpool", "Blaenau Gwent", "Bolsover", "Bolton", "Borough of Poole",
+                    "Boston", "Bournemouth", "Bracknell Forest", "Bradford", "Braintree", "Breckland", "Brent", "Brentwood", "Bridgend",
+                    "Brighton and Hove", "Bristol (City and County of)", "Broadland", "Bromley", "Bromsgrove", "Broxbourne", "Broxtowe",
+                    "Burnley", "Bury", "Caerphilly", "Calderdale", "Cambridge", "Camden", "Cannock Chase", "Canterbury", "Cardiff", 
+                    "Carlisle", "Carmarthenshire", "Castle Point", "Central Bedfordshire", "Ceredigion", "Charnwood", "Chelmsford", 
+                    "Cheltenham", "Cherwell", "Cheshire East Council", "Cheshire West & Chester", "Chesterfield", "Chichester", "Chiltern",
+                    "Chorley", "Christchurch", "City of Edinburgh", "City of London", "City of Westminster", "Clackmannan", "Colchester", 
+                    "Conwy", "Copeland", "Cornwall Council", "Cotswold", "Coventry", "Craven", "Crawley", "Croydon", "Dacorum", "Darlington",
+                    "Dartford", "Denbighshire", "Derby City", "Derbyshire Dales", "Doncaster", "Dorset", "Dover", "Dudley", "Dumfries and Galloway",
+                    "Dundee City", "Durham", "Ealing", "East Ayrshire", "East Cambridgeshire", "East Devon", "East Dunbartonshire", 
+                    "East Hampshire", "East Hertfordshire", "East Lindsey", "East Lothian", "East Renfrewshire", "East Riding of Yorkshire", 
+                    "East Staffordshire", "East Suffolk", "Eastbourne", "Eastleigh", "Eden", "Eilean Siar", "Elmbridge", "Enfield", 
+                    "Epping Forest", "Epsom and Ewell", "Exeter", "Falkirk", "Fareham", "Fenland", "Fife", "Flintshire", "Folkestone & Hythe", 
+                    "Forest of Dean", "Fylde", "Gateshead", "Gedling", "Glasgow City", "Gloucester", "Gosport", "Gravesham", "Greenwich", 
+                    "Guildford", "Gwynedd", "Hackney", "Halton", "Hambleton", "Hammersmith and Fulham", "Haringey", "Harlow", "Harrogate", 
+                    "Harrow", "Hart", "Hartlepool", "Hastings", "Havant", "Havering", "Herefordshire", "Hertsmere", "High Peak", "Highland", 
+                    "Hillingdon", "Hinckley and Bosworth", "Horsham", "Hounslow", "Huntingdonshire", "Hyndburn", "Ipswich", "Islington", 
+                    "Kensington and Chelsea", "Kingston upon Hull", "Kingston upon Thames", "Leeds", "Leicester", "Lewes", "Lewisham", 
+                    "Manchester", "Merton", "Newham", "Newport", "North Ayrshire", "North Devon", "Northumberland", "Nottingham", "Redbridge", 
+                    "Richmond upon Thames", "Southwark", "Sutton", "Tower Hamlets", "Waltham Forest", "Wandsworth", "Westminster"
                 ];
 
                 // Normalize the borough to match your list
-                borough = borough.replace(/London Borough of /, "").trim();  // Strip "London Borough of"
+                borough = borough.replace(/London Borough of /, "").replace(/Council$/, "").trim();  // Remove "London Borough of" and "Council"
 
-                // Convert to lowercase for a case-insensitive comparison
+                // Convert to lowercase for case-insensitive comparison
                 borough = borough.toLowerCase();
 
                 // Check if the borough is in the list, case-insensitively
